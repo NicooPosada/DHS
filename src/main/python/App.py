a1b91ef4dc957b4e566f71a2ad509bde5b86576f
FabricioPosada <fposada183@alumnos.iua.edu.ar>
2025-11-03 08:56:23 -0300

    Se modifico app

--- src/main/python/App.py
+++ src/main/python/App.py
@@ -1,8 +1,11 @@
 import sys
+from ErrorListener import MiErrorListener 
 from antlr4 import *
 from compiladorLexer import compiladorLexer
 from compiladorParser import compiladorParser
 from Escucha import Escucha
+
+# NOTA IMPORTANTE:
 
 # En caso de no poder ejecutar el programa Python por
 # problemas de version (error ATNdeserializer), se
@@ -16,14 +19,23 @@ def main(argv):
     if len(argv) > 1:
         archivo = argv[1]
     
-    input = FileStream(archivo)
+    input = FileStream(archivo, encoding='utf-8')
     lexer = compiladorLexer(input)
     stream = CommonTokenStream(lexer)
     parser = compiladorParser(stream)
+
+    error_listener = MiErrorListener()
+    parser.removeErrorListeners()  # Eliminar los listeners por defecto
+    parser.addErrorListener(error_listener)
+
     escucha = Escucha()
     parser.addParseListener(escucha)
     tree = parser.programa()
     #print(tree.toStringTree(recog=parser))
 
+    # Siempre mostrar el reporte de errores sintácticos
+    print(error_listener.obtener_reporte())
+
 if __name__ == '__main__':
     main(sys.argv)
+
